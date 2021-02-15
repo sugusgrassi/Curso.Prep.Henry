52f3e16c2368867f70ee65485223bac02ca7367b
sugus.grassi <sugus.grassi@gmail.com>
2021-02-15 19:07:48 -0300

    I-III completos

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -186,6 +186,16 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  seleccion = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre" ){
+      seleccion.push(array[i]);
+    }        
+  }  
+  if (seleccion.length === 3){
+    return seleccion;
+  } else {
+  } return "No se encontraron los meses pedidos";
 }
 
 
@@ -193,6 +203,12 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  mayoresACien = [];
+  for (var i = 0; i < array.length; i++){
+    if (array[i] > 100){
+      mayoresACien.push(array[i]);
+    }
+  } return mayoresACien;
 }
 
 
@@ -204,6 +220,20 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  aumentandoEnDos = [];
+  var suma = numero;
+  for (var i = 0; i < 10; i++){
+    suma = suma +2;
+    aumentandoEnDos.push(suma);
+    if ((suma+2) === i) {
+      break;
+    } 
+  } 
+  if (aumentandoEnDos.length < 10){
+    return "Se interrumpió la ejecución";
+  } else {
+    return aumentandoEnDos;
+  }
 }
 
 
@@ -214,7 +244,18 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  arrayNuevo = [];
+  var suma = numero;
+  for (var i = 0; i < 10; i++) {
+    if (i === 5){
+      continue;
+    }  else {
+      suma = suma + 2;
+      arrayNuevo.push(suma);  
+      }
+  } return arrayNuevo;
 }
+
 
 
 // No modificar nada debajo de esta línea
